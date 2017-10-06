ec300e81939d94a6128ace030d45e6b7fffe25a3
Batlijo <batlijoselevitz@gmail.com>
2017-10-06 14:02:08 -0700

    up to date file

---
+++ javascript/game.js
@@ -0,0 +1,27 @@
+
+// Have pop up window alert user to play game
+alert("Guess what letter I'm thinking of");
+
+// these are all the choices the user can enter, I arranged it this way so you don't have to scroll horizonally on this ie like this comment is doing. ;)
+var letters = ["a","b", "c", "d","e",
+				"f","g","h","i","j","k",
+				"l","m","n","o","p","q",
+				"r","s","t","u","v","w",
+				"y", "z"
+				];
+
+// need to create a way for user input answer...assign another variable
+var letterGuessed = []
+
+if letterguessed(correctGuess){
+	alert("Correct answer!");
+}	else if (wrongGuess){
+	alert("Wrong answer!");
+	}
+
+// create for loop since it nees to run through guesses. User has up to 5 guesses
+for (i=0; i<5; i++){
+	console.log(i);
+
+
+	
